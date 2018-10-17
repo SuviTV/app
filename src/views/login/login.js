2818d99a2bdbd30ec26140fbed96a6935096cc51
Brett Johnson <k.bretton.johnson@gmail.com>
2018-10-16 19:08:24 -0600

    pulling brennons code

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,24 +1,49 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import headerlogo from './headerlogo.png'
+import phoneimg from './iphoneloginimg.png'
+import fbook from './facebook.png'
 import './login.css'
 
 class login extends Component {
+
+    // componentDidMount() {
+    //     axios.get('/auth/me')
+    //         .then(resp => {
+    //             if (resp.data !== 'Not logged in!') {
+    //                 this.props.history.push('/home')
+    //             }
+    //         })
+    //         .catch(err => {
+    //             console.error(err)
+    //         })
+    // }
+
     render(){
         return(
-            
-            <div className='container'>
-                <div className='title'>
-                    <p>Welcome to Suvi</p>
+            <div>
+                <div className='header'>
+                    <img className='headerlogo' src={headerlogo}/>
                 </div>
-                <div className='module'>
-                    <button className='facebook'><img src='/facebook.jpg'/>Facebook</button>
-                    <br/>
-                    <div className='line'></div>
-                    <br/>
-                    <button className='login'>Login</button>
-                    <br/>
-                    <button className='createAccount'>Create Account</button>
-                    <br/>
-                    <p>Forgot Password?</p>
+                <div className='container'>
+                    <div className='iphoneimgdiv'>
+                        <img className='iphone' src={phoneimg}/>
+                    </div>
+                    <div className='module'>
+                        <h1 className='welcome'>Welcome to Suvi</h1>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <button className='facebook'><img src={fbook}/>Login with Facebook</button>
+                        <br/>
+                        <div className='line'></div>
+                        <br/>
+                        <button className='login'>Login</button>
+                        <br/>
+                        <button className='createAccount'>Create Account</button>
+                        <br/>
+                    </div>
                 </div>
             </div>
         
